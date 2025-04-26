dad96b64477ab3005ca849097663cc0bcee04597
AZhou0102 <albert.zhou0412@gmail.com>
2025-04-26 16:46:29 -0400

    Docstring update for solve_murnaghan.py

--- solve_murnaghan.py
+++ solve_murnaghan.py
@@ -11,11 +11,36 @@ import numpy as np
 
 #computes pressure from density via murnaghan. Used for checking density guesses
 def murnaghan(rho, rho_0, B_0, B_01):
+    """Calculate pressure using the Murnaghan equation of state.
+    
+    Parameters:
+    rho (float): Density at pressure P (kg/m^3).
+    rho_0 (float): Initial (zero-pressure) density (kg/m^3).
+    B_0 (float): Bulk modulus at zero pressure (Pa).
+    B_01 (float): Pressure derivative of the bulk modulus.
+
+    Returns:
+    float: Pressure corresponding to the given density (Pa).
+    """
     pressure = (B_0/B_01)*((rho_0/rho)**(-1*B_01)-1)
     return(pressure)
 
 #takes a target pressure and returns the density associated with it via murnaghan
 def invert_murnaghan(P_target, rho_0, rho_min, rho_max, B_0, B_01, allowed_discrepancy = (0.05 * (10 ** 9))):
+    """Iteratively calculates density corresponding to a target pressure using the Murnaghan equation.
+    
+    Parameters:
+    P_target (float): The target pressure (Pa).
+    rho_0 (float): Initial (zero-pressure) density (kg/m^3).
+    rho_min (float): Minimum density guess (kg/m^3).
+    rho_max (float): Maximum density guess (kg/m^3).
+    B_0 (float): Bulk modulus at zero pressure (Pa).
+    B_01 (float): Pressure derivative of the bulk modulus.
+    allowed_discrepancy (float): Maximum allowed difference between the calculated and target pressure (Pa).
+    
+    Returns:
+    float: The calculated density corresponding to the target pressure.
+    """
     rho_guess = (rho_min + rho_max) / 2
     P_guess = murnaghan(rho_guess, rho_0, B_0, B_01)
 
@@ -47,6 +72,12 @@ def make_input(target_pressures, parameters):
 
 #list parameters in order of: rho_0, rho_min, rho_max, B_0, B_01
 def display_invert_murnaghan(target_pressures, parameters):
+    """Display the density corresponding to each target pressure using the Murnaghan equation.
+    
+    Parameters:
+    target_pressures (list): A list of target pressures (Pa).
+    parameters (list): A list of parameters to be passed to invert_murnaghan.
+    """
     for key, value in make_input(target_pressures, parameters).items():
         print(invert_murnaghan(*value))
 
